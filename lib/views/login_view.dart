312626ca74f87dfcc8425192c367dce61aa10e89
Vladislav <patchstudiocompanyv@gmail.com>
2025-01-06 15:08:46 +0200

    step-24

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -4,6 +4,7 @@ import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/services/auth/auth_exceptions.dart';
 import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
 import 'package:mynotes/services/auth/bloc/auth_event.dart';
+import 'package:mynotes/services/auth/bloc/auth_state.dart';
 import 'package:mynotes/utilities/dialogs/error_dialog.dart';
 
 class LoginView extends StatefulWidget {
@@ -57,33 +58,38 @@ class _LoginViewState extends State<LoginView> {
               hintText: 'Password',
             ),
           ),
-          TextButton(
-              onPressed: () async {
-                final email = _email.text;
-                final password = _password.text;
-                try {
-                  context.read<AuthBloc>().add(AuthEventLogIn(
-                        email,
-                        password,
-                      ));
-                } on InvalidCredentialAuthException {
+          BlocListener<AuthBloc, AuthState>(
+            listener: (context, state) async {
+              if (state is AuthStateLoggedOut) {
+                if (state.exception is InvalidCredentialAuthException) {
                   await showErrorDialog(
                     context,
-                    'Invalid credential',
+                    'Invalid credentials',
                   );
-                } on WrongPasswordAuthException {
+                } else if (state.exception is WrongPasswordAuthException) {
                   await showErrorDialog(
                     context,
-                    'Wrong password',
+                    'Wrong credentials',
                   );
-                } on GenericAuthException {
+                } else if (state.exception is GenericAuthException) {
                   await showErrorDialog(
                     context,
                     'Authentification error',
                   );
                 }
-              },
-              child: const Text('Login')),
+              }
+            },
+            child: TextButton(
+                onPressed: () async {
+                  final email = _email.text;
+                  final password = _password.text;
+                  context.read<AuthBloc>().add(AuthEventLogIn(
+                        email,
+                        password,
+                      ));
+                },
+                child: const Text('Login')),
+          ),
           TextButton(
             onPressed: () {
               Navigator.of(context).pushNamedAndRemoveUntil(
